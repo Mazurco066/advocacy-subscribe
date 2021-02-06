313df054211770d32e8e01180b24fa6901bbbc94
Gabriel Ribeiro <mazurco066@gmail.com>
2021-02-06 10:51:00 -0300

    feat(quoted): added quoted section

--- components/Quoted/index.js
+++ components/Quoted/index.js
@@ -2,14 +2,39 @@
 import React from 'react'
 
 // Components
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 
 // Component
 export const Quoted = () => {
   return (
-    <section>
+    <section id="quoted-section">
       <Container>
-        <p>Quoted - Under development</p>
+        <Row>
+          <Col md="12" className="d-flex justify-content-center mb-3">
+            <div className="quote-image">
+              <img src="/images/quotes.png" alt="Quote"/>
+            </div>
+          </Col>
+          <Col md="12" className="mb-5">
+            <h2 className="text-center text-uppercase">
+              Nós tempos a experiência e a confiança que você procura
+            </h2>
+          </Col>
+          <Col md="12" className="pt-3">
+            <p className="text-justify quote-text mb-5">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <h5 className="text-center">
+              (X Pessoa falou isso)
+            </h5>
+          </Col>
+        </Row>
       </Container>
     </section>
   )
